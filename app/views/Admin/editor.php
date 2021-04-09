286eb4ede7d244f3f36d02d2f7d5e2f255caa344
bwpi <bwpi@yandex.ru>
2021-04-09 09:09:55 +0500

    main

--- app/views/Admin/editor.php
+++ app/views/Admin/editor.php
@@ -1,13 +1,13 @@
 <?php
 
-function renderFiles($dataset) {
-    echo '<ul class="ml-3 list-group p-1">';        
-    foreach ($dataset as $key => $value) {        
-        if(gettype($dataset[$key]) !== 'array'){
-            echo "<li class='list-group-item text-info'><a href='/{$key}/{$value}'><i class='fa fa-file-text' aria-hidden='true'></i> {$value}</a></li>";
-        } else {
+function renderFiles($dataset) {    
+    echo '<ul class="ml-3 list-group p-1">';
+    foreach ($dataset as $key => $value) {
+        if(gettype($value) !== 'array'){            
+            echo "<li class='list-group-item text-info'><a class='btn btn-info' href='{$value}'><i class='mr-1 fa fa-file-text' aria-hidden='true'></i>{$value}</a></li>";
+        } else {            
             echo "<li class='list-group-item'>";
-            echo "<div class='h5'><i class='fa fa-folder-open' aria-hidden='true'></i> {$key}</div>";
+            echo "<div class='h5'><i class='mr-1 fa fa-folder-open' aria-hidden='true'></i>{$key}</div>";
             renderFiles($value);
             echo "</li>";
         }
@@ -17,6 +17,5 @@ function renderFiles($dataset) {
 
 ?>
 <div class="h3">Редактор данных системы</div>
-<div class="h4 text-center">Обзор файлов <i class="fa fa-sitemap" aria-hidden="true"></i></div>
+<div class="h4 text-center">Обзор файлов <i class="mr-1 fa fa-sitemap" aria-hidden="true"></i></div>
 <?php renderFiles($dataset);?>
-
